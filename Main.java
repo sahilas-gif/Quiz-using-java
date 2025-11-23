81851c833c94c18ba31c1409506f1d9b9ffebc33
sahilas-gif <sahil.as@somaiya.edu>
2025-11-23 12:39:29 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String a[]) {
-        QuestionService service = new QuestionService();
-        service.playQuiz();
-        service.printScore();
-    }
-}
